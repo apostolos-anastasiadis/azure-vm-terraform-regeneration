db7530e192480eb1b8531c14950efbfafbce3302
apostolos-anastasiadis <128220836+apostolos-anastasiadis@users.noreply.github.com>
2023-04-22 21:23:10 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ terraform{
 
 #Define provider configurations
 provider "azurerm" {
+  features {}
   subscription_id = "3d462945-0710-4a95-9a75-a637f18d384a"
   client_id       = "e9a43270-5365-4f22-acc1-13bfc2558829"
   client_secret   = "O_N8Q~CzY4Ai7j-RUzqX5oK25yQZg.w694fDJalw"
