9cf58fd7700d36eb1e126276d67bc58789ecd6e2
apostolos-anastasiadis <128220836+apostolos-anastasiadis@users.noreply.github.com>
2023-04-23 21:23:52 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,6 +72,17 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+   security_rule {
+    name                        = "HTTP-8080"
+    priority                    = 110
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "8080"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
 }
 
 # Create network interface
